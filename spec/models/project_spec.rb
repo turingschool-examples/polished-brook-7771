4aadf28e84c10cec53bce3f2e84ba4d2cf905bf3
Derek Chavez <frrenose17@yahoo.com>
2023-07-14 15:03:30 -0600

    feat: contestant_count method to project model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -11,4 +11,23 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe "#contestant_count" do
+    it "returns the number of contestants that belong to a project" do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+      erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+      news_chic.contestants << jay
+      news_chic.contestants << gretchen
+      news_chic.contestants << kentaro
+      news_chic.contestants << erin
+
+      expect(news_chic.contestant_count).to eq(4)
+    end
+  end
 end
