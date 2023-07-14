e47271515c20a1e1f53efc38472cd94ef30d3c15
Ian Lyell <ian.lyell@yahoo.com>
2023-07-14 15:35:13 -0500

    Add test for user story 2 index page

---
+++ spec/features/contestants/index_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe "Contestants index page", type: :feature do
+    before(:each) do
+        @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+        @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+        @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+        @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+        @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+        @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+        @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+        @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+        @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+        @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+
+        ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+        ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+        ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+        ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+        ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+        ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
+
+
+    end
+    describe "As a visitor" do
+        describe "When I viist the contestants index page ('/contestants')" do
+            it "Shows a list of names of all the contestants and their projects" do
+
+        
+
+                visit "/contestants"
+
+                expect(page).to have_content(jay.name)
+                expect(page).to have_content(gretchen.name)
+                expect(page).to have_content(kentaro.name)
+                expect(page).to have_content(erin.name)
+
+                expect(page).to have_content(@news_chic.name)
+                expect(page).to have_content(@news_chic.name)
+                expect(page).to have_content(@upholstery_tux.name)
+                expect(page).to have_content(@upholstery_tux.name)
+                expect(page).to have_content(@boardfit.name)
+                expect(page).to have_content(@boardfit.name)
+
+            end
+        end
+    end
+end
