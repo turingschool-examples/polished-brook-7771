543ac4f6f10fc604487712f20ea6e6729cbd9999
Derek Chavez <frrenose17@yahoo.com>
2023-07-14 13:41:16 -0600

    Get tests to pass and starton user story 1, Started IC at 1:35pm

---
+++ spec/features/project_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "project show page" do
+
+  # User Story 1 of 3
+  # As a visitor,
+  # When I visit a project's show page ("/projects/:id"),
+  # I see that project's name and material
+  # And I also see the theme of the challenge that this project belongs to.
+  # (e.g.    Litfit
+  #     Material: Lamp Shade
+  #   Challenge Theme: Apartment Furnishings)
+
+  describe "when I visit a project's show page" do
+    it "displays that projects name and materail" do
+      
+    end
+  end
+end
