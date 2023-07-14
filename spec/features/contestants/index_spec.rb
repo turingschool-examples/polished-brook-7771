c9a6da4017471e8f1ae309afe395745a8a2756b2
Derek Chavez <frrenose17@yahoo.com>
2023-07-14 14:43:06 -0600

    feat: add project names to contestants index page

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -30,5 +30,38 @@ RSpec.describe "contestants index page" do
       expect(page).to have_content(kentaro.name)
       expect(page).to have_content(erin.name)
     end
+
+    it "displays under each contestant a list of project names that they have participated in" do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+      
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+      erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+      upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+      lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+      jay.projects << news_chic
+      gretchen.projects << news_chic
+      gretchen.projects << boardfit
+      kentaro.projects << upholstery_tux
+      kentaro.projects << lit_fit
+      erin.projects << lit_fit
+
+      visit "/contestants"
+
+      expect(jay.projects.first).to eq(news_chic)
+      expect(gretchen.projects.first).to eq(news_chic)
+      expect(kentaro.projects.first).to eq(upholstery_tux)
+      expect(erin.projects.first).to eq(lit_fit)
+
+      expect(page).to have_content(news_chic.name)
+      
+    end
   end
 end
