4f9a97abcc5249ffdb24b6129255b9eb2d5db6d9
Artemy Gibson <artemygibson@gmail.com>
2023-07-16 15:05:42 -0700

    Test render_projects method

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -12,4 +12,25 @@ RSpec.describe Contestant, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
   end
+
+  describe "#render_projects" do
+    it "returns name of projects for a given contestant" do
+      @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+  
+      @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+  
+      @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+  
+      @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+      ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+      
+      expect(@jay.render_projects).to eq("News Chic")
+      expect(@gretchen.render_projects).to eq("News Chic, Upholstery Tuxedo")
+    end
+  end
 end
