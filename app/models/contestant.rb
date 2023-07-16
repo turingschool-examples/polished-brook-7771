6611250bb31624caa3ec45ca63d64ccdc5ce5c94
Artemy Gibson <artemygibson@gmail.com>
2023-07-16 15:03:39 -0700

    Add render_projects method

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -2,4 +2,12 @@ class Contestant < ApplicationRecord
   validates_presence_of :name, :age, :hometown, :years_of_experience
   has_many :contestant_projects
   has_many :projects, through: :contestant_projects
+
+  def render_projects
+    names = []
+    self.projects.each do |project|
+      names << project.name
+    end
+    names.join(", ")
+  end
 end
