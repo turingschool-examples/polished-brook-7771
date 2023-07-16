42a6d3f03f2cdb8c2ff6c4c070c2202e0375f2cf
Allen Russell <garyrusselljr@gmail.com>
2023-07-16 11:46:54 -0600

    test: Add test for project show page, route, and wip view

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "project show page" do
+  before(:each) do
+    @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+    @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+    @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+  end
+
+  describe "as a visitor" do
+    describe "when I visit a project show page" do
+      it "I see the theme of the challenge that this project belongs to " do
+
+        visit "/projects/#{@lit_fit.id}"
+
+        expect(page).to have_content("Litfit")
+        expect(page).to have_content("Lamp")
+        expectd(page).to have_content("Apartment Furnishings")
+      end
+    end
+  end
+end
