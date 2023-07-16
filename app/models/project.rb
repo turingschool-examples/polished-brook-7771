b7c33b5f9bdb4f0dbbe34092bdc541d868bcf5c6
Artemy Gibson <artemygibson@gmail.com>
2023-07-16 16:19:07 -0700

    Add average_contestant_experience method

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,4 +7,13 @@ class Project < ApplicationRecord
   def count_contestants
     Contestant.joins(:projects).where(projects: {id: self.id}).count
   end
+
+  def average_contestant_experience
+    years = Contestant.joins(:projects).where(projects: {id: self.id}).average(:years_of_experience)
+    return "N/A" if years.nil?
+    # any active record methods that return a string if nil?
+    years.round(1)
+    # put round in view originally since it's "display logic"
+    # got error that round doesn't work on "N/A" string
+  end
 end
