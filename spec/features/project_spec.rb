0b48335520f4c4c1d5eed5dbb12ef768e133c540
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-16 18:51:49 -0400

    test: adds test for user story 3

--- spec/features/project_spec.rb
+++ spec/features/project_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe 'Projects', type: :feature do
       visit projects_path
 
       expect(page).to have_link('Challenges')
-      expect(page).to have_link('Contestants Projects')
       expect(page).to have_link('Contestants')
       expect(page).to have_link('Projects')
     end
@@ -31,6 +30,27 @@ RSpec.describe 'Projects', type: :feature do
       expect(page).to have_content(@project1.name)
       expect(page).to have_content(@project1.material)
       expect(page).to have_content(@challenge1.theme)
+    end
+
+    # User Story 3
+    it 'displays the number of contestants on the project show page' do
+      @challenge1 = Challenge.create!(id: 1, theme: 'Recycled Material', project_budget: 1000)
+      @project1 = Project.create!(id: 1, name: 'News Chic', material: 'Newspaper', challenge_id: 1)
+      @project2 = Project.create!(id: 2, name: 'Upholstery Tuxedo', material: 'Couch', challenge_id: 1)
+      @contestant1 = Contestant.create!(id: 1, name: 'Kentaro Kameyama', age: 32, hometown: 'Denver, CO',
+                                        years_of_experience: 10)
+      @contestant2 = Contestant.create!(id: 2, name: 'Jay McCarroll', age: 40, hometown: 'Denver, CO',
+                                        years_of_experience: 10)
+
+      @contestant1.projects << @project1
+      @contestant1.projects << @project2
+      @contestant2.projects << @project2
+
+      visit project_path(@project1)
+      expect(page).to have_content('Number of Contestants: 1')
+
+      visit project_path(@project2)
+      expect(page).to have_content('Number of Contestants: 2')
     end
   end
 end
