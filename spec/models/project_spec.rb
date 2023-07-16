475aaae2b1e83978f344579ef9f64e382759cbb4
Artemy Gibson <artemygibson@gmail.com>
2023-07-16 16:21:40 -0700

    Test average_contestants_experience method

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,8 +12,7 @@ RSpec.describe Project, type: :model do
     it {should have_many(:contestants).through(:contestant_projects)}
   end
 
-  describe "count_contestants" do
-    it "can count contestants belonging to a project" do
+  before do
     @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
     @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
 
@@ -24,7 +23,7 @@ RSpec.describe Project, type: :model do
 
     @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
     @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
-    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 9)
     @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
 
 
@@ -33,10 +32,21 @@ RSpec.describe Project, type: :model do
     ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
     ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
     ContestantProject.create(contestant_id: @jay.id, project_id: @boardfit.id)
+  end
 
-    expect(@news_chic.count_contestants).to eq(2)
-    expect(@boardfit.count_contestants).to eq(3)
-    expect(@lit_fit.count_contestants).to eq(0)
+  describe "count_contestants" do
+    it "can count contestants belonging to a project" do
+      expect(@news_chic.count_contestants).to eq(2)
+      expect(@boardfit.count_contestants).to eq(3)
+      expect(@lit_fit.count_contestants).to eq(0)
+    end
+  end
+
+  describe "average_contestant_experience" do
+    it "calculates average years of experience" do
+      expect(@news_chic.average_contestant_experience).to eq(12.5)
+      expect(@lit_fit.average_contestant_experience).to eq("N/A")
+      expect(@boardfit.average_contestant_experience).to eq(12.3)
     end
   end
 end
