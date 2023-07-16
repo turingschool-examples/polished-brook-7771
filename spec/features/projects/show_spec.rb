97baaabab3bf0d592bf7de4de1d711e0f4be021d
Artemy Gibson <artemygibson@gmail.com>
2023-07-16 15:55:50 -0700

    Test that page renders number of contestants

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -3,7 +3,24 @@ require "rails_helper"
 RSpec.describe "Project show page" do
   before do
     @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
     @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
+    ContestantProject.create(contestant_id: @jay.id, project_id: @boardfit.id)
   end
 #   User Story 1 of 3
 
@@ -26,6 +43,29 @@ RSpec.describe "Project show page" do
     visit "/projects/#{@news_chic.id}"
 
     expect(page).to have_content("Challenge Theme: Recycled Material")
-    save_and_open_page
+  end
+
+#   User Story 3 of 3
+# As a visitor,
+# When I visit a project's show page
+# I see a count of the number of contestants on this project
+
+# (e.g.    Litfit
+#     Material: Lamp Shade
+#   Challenge Theme: Apartment Furnishings
+#   Number of Contestants: 3 )
+
+  it "has a count of how many contestants are on this project" do
+    visit "/projects/#{@news_chic.id}"
+
+    expect(page).to have_content("Number of Contestants: 2")
+
+    visit "/projects/#{@lit_fit.id}"
+    
+    expect(page).to have_content("Number of Contestants: 0")
+
+    visit "/projects/#{@boardfit.id}"
+
+    expect(page).to have_content("Number of Contestants: 3")
   end
 end
