28b4533e6bd3c9b2cbec4c31f43b08358333f099
Chris Cullinane <chriscullinane@protonmail.com>
2023-07-15 23:38:02 -0800

    removing comments

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -32,17 +32,3 @@ RSpec.describe "Contestants index page" do
     
   end
 end
-
-
-# User Story 2 of 3
-
-# As a visitor,
-# When I visit the contestants index page ("/contestants")
-# I see a list of names of all the contestants
-# And under each contestants name I see a list of the projects (names) that they've been on
-
-# (e.g.   Kentaro Kameyama
-#         Projects: Litfit, Rug Tuxedo
-
-#         Jay McCarroll
-#         Projects: LeatherFeather)
