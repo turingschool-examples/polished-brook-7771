d1abb2bae0ff0bf97cb68df46b866cbd22106927
Kaina Cockett <kainacockett@gmail.com>
2023-07-17 10:39:55 -0600

    feat: US2 contestants index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "projects/:id", to: "projects#show"
+  get "/projects/:id", to: "projects#show"
+
+  get "/contestants", to: "contestants#index"
 end
