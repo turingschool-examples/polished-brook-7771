786374c75492d068d38c6e3d6cd069ae6af07c8f
Jorja Fleming <aset284@gmail.com>
2023-07-17 16:30:23 -0600

    Feat: Add story 4

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,10 @@ class Project < ApplicationRecord
   belongs_to :challenge
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
+
+
+  def average_experience
+    total_experience = contestants.sum(&:years_of_experience)
+    (total_experience / contestants.count.to_f).round(2)
+  end
 end
--- spec/features/project_spec.rb
+++ spec/features/project_spec.rb
@@ -12,4 +12,51 @@ RSpec.feature "Projects", type: :feature do
     expect(page).to have_content(project_1.name)
     expect(page).to have_content(project_1.material)
   end
+
+#   User Story 3 of 3
+# As a visitor,
+# When I visit a project's show page
+# I see a count of the number of contestants on this project
+
+# (e.g.    Litfit
+#     Material: Lamp Shade
+#   Challenge Theme: Apartment Furnishings
+#   Number of Contestants: 3 )
+
+  it "can see a count of the number of contestants on this project" do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12) 
+    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+    visit "/projects"
+
+    expect(page).to have_content("Number of Contestants: 2")
+  end
+
+#     As a visitor,
+# When I visit a project's show page
+# I see the average years of experience for the contestants that worked on that project
+# (e.g.    Litfit
+#     Material: Lamp Shade
+#   Challenge Theme: Apartment Furnishings
+#   Number of Contestants: 3
+#   Average Contestant Experience: 10.25 years)
+
+  it "can see the average years of experience for the contestants that worked on that project" do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    
+    visit "/project/#{news_chic.id}"
+
+    expect(page).to have_content(news_chic.name)
+    expect(page).to have_content(news_chic.material)
+    expect(page).to have_content("Average Contestant Experience: 13.0 years")
+  end
 end
