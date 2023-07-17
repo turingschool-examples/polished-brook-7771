13feab0c27b03cde13ccd03150fb0d9569359f4e
Dylan Timmons <dylanstimmons@icloud.com>
2023-07-16 18:48:45 -0600

    Add contestant model testing

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe Contestant, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
   end
+
+  describe '#project_names' do
+    it 'returns a list of project names' do 
+      challenge_1 = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      contestant_1 = Contestant.create!(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      project_1 = contestant_1.projects.create!(name: "News Chic", material: "Newspaper", challenge: challenge_1)
+      project_2 = contestant_1.projects.create!(name: "Upholstery Tuxedo", material: "Couch", challenge: challenge_1)
+
+      expected_project_names = "#{project_1.name}, #{project_2.name}"
+      expect(contestant_1.project_names).to eq(expected_project_names)
+    end
+  end
+
+
 end
