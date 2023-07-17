52c9442fe64731665901bbb4695e69c2347297a9
Kaina Cockett <kainacockett@gmail.com>
2023-07-17 11:21:31 -0600

    test: model method test average_years_experience

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,30 @@
 require "rails_helper"
 
 RSpec.describe Project, type: :model do
+
+  before(:each) do
+    # Challenges =================================================================
+    @recycled_material_challenge = Challenge.create!(theme: "Recycled Material", project_budget: 1000)
+    @furniture_challenge = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1000)
+    # Projects =================================================================
+    @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+    @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+    # Contestants =================================================================
+    @jay = Contestant.create!(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create!(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create!(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create!(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+    ContestantProject.create!(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create!(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    ContestantProject.create!(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+    ContestantProject.create!(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+    ContestantProject.create!(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    ContestantProject.create!(contestant_id: @erin.id, project_id: @boardfit.id)
+  end
+
   describe "validations" do
     it {should validate_presence_of :name}
     it {should validate_presence_of :material}
@@ -10,5 +34,18 @@ RSpec.describe Project, type: :model do
     it {should belong_to :challenge}
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
+  end
+
+  describe '#average_years_experience' do
+    it 'returns 0 when there are no associated contestants' do
+      blank_project = Project.new
+      expect(blank_project.average_years_experience).to eq(0)
+    end
+
+    it 'calculates the average years of experience for associated contestants' do
+      @boardfit.contestants = [@jay, @gretchen]
+
+      expect(@boardfit.average_years_experience).to eq(12.5)
+    end
   end
 end
