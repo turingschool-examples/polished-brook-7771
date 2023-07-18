500393472c8c3a10c39198f4349a9d0f55862e2b
Parker Boeing <parker.boeing@gmail.com>
2023-07-18 03:36:10 -0600

    feat / test: add feature and test for average_experience_of_contestants method to project model

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
+    require 'pry'; binding.pry
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,12 @@ class Project < ApplicationRecord
   belongs_to :challenge
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
+
+  def average_experience_of_contestants
+    total_experience = 0
+    contestants.each do |contestant|
+      total_experience += contestant.years_of_experience
+    end
+    (total_experience.to_f / contestants.count).round(1)
+  end
 end
