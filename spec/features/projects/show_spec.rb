2b042f6659691a10b6ed20e946b0c59ca0b9bf46
Gabriel Torres <thomastgtorres21@gmail.com>
2023-07-17 21:25:33 -0500

    add test for user story 1

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -6,26 +6,33 @@ require 'rails_helper'
 # I see that project's name and material
 
 
+
+describe "projects show page" do
+  before :each do
+    @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+    
+    @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+  end
+  describe "when I visit a show page" do 
+    it "shows the projects name and material"do 
+    visit "/projects/#{@news_chic.id}"
+    
+    expect(page).to have_content(@news_chic.name)
+    expect(page).to have_content(@news_chic.material) 
+  end
+    it "shows name of theme" do 
+      visit "/projects/#{@news_chic.id}"
+
+      expect(page).to have_content(@news_chic.challenge.theme)
+    end
+end
 # And I also see the theme of the challenge that this project belongs to.
 # (e.g.    Litfit
 #     Material: Lamp Shade
 #   Challenge Theme: Apartment Furnishings)
 
-  describe "projects show page" do
-    before :each do
-      @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-      @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
-      
-      @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-      @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
-    end
-    describe "when I visit a show page" do 
-      it "shows the projects name material"do 
-        visit "/projects/#{@news_chic.id}"
-        
-        expect(page).to have_content(@news_chic.name)
-        expect(page).to have_content(@news_chic.material) 
-      end
-    end
-  end
+
+end
 
