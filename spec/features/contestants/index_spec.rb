b3a9718d2bdb85f8104a8f5fa3a74a1be9ca6ba2
Gabriel Torres <thomastgtorres21@gmail.com>
2023-07-17 22:41:37 -0500

    rearrange instance variables.. -___-

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -2,25 +2,33 @@ require 'rails_helper'
 
 RSpec.describe 'contestants index page' do 
   before :each do 
-    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
-    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
-    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
-    erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+    @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
     
-    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
-    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
-    ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
-    ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
-    ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
-    ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+    @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+    
+    @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+    
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
 
     
-    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+    # @jay.projects << @news_chic
+    # @gretchen.projects << @boardfit
+    # @erin.projects << @lit_fit
+    
   end
-
-
-  
   #   User Story 2 of 3
   
   # As a visitor,
@@ -42,6 +50,7 @@ RSpec.describe 'contestants index page' do
   it "also lists each contestants projects" do 
     visit "/contestants"
 
+    save_and_open_page
     expect(page).to have_content("News Chic")
     expect(page).to have_content("Boardfit")
   end
