babfe4caec7d1d22843fe35142dc03ce4cbd440c
Bradley Milton <bmilton2412@gmail.com>
2023-07-18 07:58:21 -0700

    Adds user story 3 and tests

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -12,13 +12,29 @@ RSpec.describe "Project's show page" do
     expect(page).to have_content("Material: #{news_chic.material}")
     expect(page).to have_content("Challenge Theme: #{recycled_material_challenge.theme}")
   end
+
+  it "should show a count of contestants on this project" do
+
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+    visit "/projects/#{news_chic.id}"
+
+    expect(page).to have_content("Number of Contestants: 2")
+  end
 end
 
 
+# User Story 3 of 3
 # As a visitor,
-# When I visit a project's show page ("/projects/:id"),
-# I see that project's name and material
-# And I also see the theme of the challenge that this project belongs to.
+# When I visit a project's show page
+# I see a count of the number of contestants on this project
+
 # (e.g.    Litfit
 #     Material: Lamp Shade
-#   Challenge Theme: Apartment Furnishings)
+#   Challenge Theme: Apartment Furnishings
+#   Number of Contestants: 3 )
