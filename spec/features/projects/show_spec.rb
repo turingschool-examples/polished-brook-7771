eb030a05eec3c7b0818823c4509a50e990474b33
Parker Boeing <parker.boeing@gmail.com>
2023-07-18 02:49:25 -0600

    test: User story 1

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'projects page' do
+  before :each do
+    @recycled_material_challenge = Challenge.create!(theme: "Recycled Material", project_budget: 1000)
+    @furniture_challenge = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1000)
+    
+    @news_chic = @recycled_material_challenge.projects.create!(name: "News Chic", material: "Newspaper")
+    @boardfit = @recycled_material_challenge.projects.create!(name: "Boardfit", material: "Cardboard Boxes")
+    
+    @upholstery_tux = @furniture_challenge.projects.create!(name: "Upholstery Tuxedo", material: "Couch")
+    @lit_fit = @furniture_challenge.projects.create!(name: "Litfit", material: "Lamp")
+    
+    @jay = Contestant.create!(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create!(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create!(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create!(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+    
+    
+    ContestantProject.create!(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create!(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    ContestantProject.create!(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+    ContestantProject.create!(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+    ContestantProject.create!(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    ContestantProject.create!(contestant_id: @erin.id, project_id: @boardfit.id)
+  end
+
+
+  describe 'When I visit projects/:id' do
+    it 'shows the route with the corresponding id and all its attributes' do
+      visit "/projects/#{@news_chic.id}"
+      expect(page).to have_content(@news_chic.name)
+      expect(page).to have_content(@news_chic.material)
+      expect(page).to have_content(@boardfit.name)
+      expect(page).to have_content(@boardfit.material)
+      expect(page).to have_content(@recycled_material_challenge.material)
+
+      expect(page).to_not have_content(@upholstery_tux.name)
+      expect(page).to_not have_content(@upholstery_tux.material)
+      expect(page).to_not have_content(@lit_fit.name)
+      expect(page).to_not have_content(@lit_fit.material)
+      expect(page).to_not have_content(@furniture_challenge.material)
+    end
+  end
+end
