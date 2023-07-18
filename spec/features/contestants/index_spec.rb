6c20f53dd81625acb451e8d88946c7ea40826ade
Gabriel Torres <thomastgtorres21@gmail.com>
2023-07-17 21:54:37 -0500

    add more testing for contestants index page

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -6,6 +6,17 @@ RSpec.describe 'contestants index page' do
     gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
     kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
     erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+    
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+    ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+
+    
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
   end
 
 
@@ -26,5 +37,12 @@ RSpec.describe 'contestants index page' do
 
     expect(page).to have_content("Jay McCarroll")
     expect(page).to have_content("Erin Robertson")
+  end
+
+  it "also lists each contestants projects" do 
+    visit "/contestants"
+
+    expect(page).to have_content("News Chic")
+    expect(page).to have_content("Boardfit")
   end
 end
