ce490233a3c2088279f98d4d73fb6db4c1be82b1
Anna Wiley <awiley33@gmail.com>
2023-07-18 09:48:18 -0500

    feat: Add functionality for extension 1

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,15 @@ class Project < ApplicationRecord
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
 
+  def avg_experience
+    years = []
+    self.contestants.each do |contestant|
+      years << contestant.years_of_experience.to_f
+    end
+    if years.count != 0
+      (years.sum)/(years.count)
+    else
+      0
+    end
+  end
 end
